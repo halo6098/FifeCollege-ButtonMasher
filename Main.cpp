497efe4d2878c7acd665e47bf39b50ba99433952
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-09-05 15:38:29 +0100

    Set up project for SFML, added Main function

---
+++ Main.cpp
@@ -0,0 +1,27 @@
+// Included Libraries
+#include <SFML/Graphics.hpp>
+
+
+
+// entry point for the program
+int main()
+{
+	// Make a variable called gameWindow of type RenderWindow
+	sf::RenderWindow gameWindow;
+
+
+
+	// Game Loop
+	while (true)
+	{
+		// TODO: Check for input
+
+		// TODO: Update game state
+
+		// TODO: Draw graphics
+
+	}
+
+	// exit point for the program
+	return 0;
+}
